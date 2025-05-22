0e9100d3043c12fbe8037376f47005896ea7b6ee
ALEXIS <anavarro776@gmail.com>
2025-05-22 16:48:35 -0600

    Class Assaginmet

--- register.js
+++ register.js
@@ -1,65 +1,59 @@
 console.log("---Register Form---");
 
-// Creating Objects Literals
+let pets=[];
 
-let pet1 = {
-    name:"Luna",
-    age:"2 years old",
-    gender:"Female",
-    service:"",
-    breed:"German Shepard "
+let inputName = document.getElementById("txtName");
+let inputAge = document.getElementById("txtAge");
+let inputGender = document.getElementById("txtGender");
+let inputBreed = document.getElementById("txtBreed");
+let inputService = document.getElementById("selectService");
+//Creating Obj Constructor
+function Pet(name,age,gender,breed,service){
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+    this.breed=breed;
+    this.service=service;
 
-    
-}
-
-let pet2 ={
-     name:"Choco",
-    age:" 3 years old",
-    gender:"Female",
-    service:"",
-    breed:"Chihuaua"
-}
-
-let pet3= {
-     name:"Zorro",
-    age:"1 year old",
-    gender:"Male",
-    service:"",
-    breed:"Belgian Shepard"
 
 }
 
-//Creates Array for pets
-    let petsList= [];
+function register(){
+    let newPet= Pet(inputName.value,inputAge.value,inputGender,inputBreed.value);
+    pets.push(newPet);
+    console.log(newPet);
+    alert("The Registeration was successful");
+     displayInfo();
+}
 
-    petsList.push(pet1,pet2,pet3);
+function clearForm(){
+    document.getElementById("txtName").value="";
+    document.getElementById("txtAge").value="";
+    document.getElementById("txtGender").value="";
+    document.getElementById("txtBreed").value="";
+     document.getElementById("selectService").value="";
 
 
+}
 
-// Counts  The Pets
-function petCount(){
-    console.log(petsList.length);
-    document.getElementById("Count").innerHTML=`<p>The Total Of Pets Is ${petsList.length}<p>`;
+function displayInfo(){
+    console.log(pets.length);
+    document.getElementById("info") .innerHTML= `<p>Total Pets Is :${pets.length}</p>`;
 }
 
 
 
-    
-// Gets Pet Names
-function getPetNames(){
-   
-    let list =  document.getElementById ("List") ;
-    for(let i=0;i<petsList.length;i++){
-        console.log(`Name:${petsList[i].name}`);
-        list.innerHTML+=`<li> ${petsList[i].name}</li>`;
-       
-    }
+function init(){
+    //Creates obj
+    let pet1= new Pet("Shaggy",19,"Male");
+  
+    let pet2= new Pet("Choco",7,"Male","Mix");
     
    
-    
+    let pet3 =new Pet( "twitty",23,"Male","Canaarian");
+      pets.push(pet1,pet2,pet3);
+    console.log(pets);
+    displayInfo();
 }
 
-getPetNames();
-petCount();
-
-
+window.onload=init;
